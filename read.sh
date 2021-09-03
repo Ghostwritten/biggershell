a671bc7ab3e9973a3283e3360003315ac7cda421
Ghostwritten <13475639563@163.com>
2021-09-03 15:02:49 +0800

    Create read.sh

---
+++ read.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#----------------------------------
+# FUNCTION: read 
+# AUTHER: 1zoxun1@gmail.com
+# DATE: 2021-09-03 23:58:56 
+#-----------------------------------
+
+echo -n "Enter \"a\" " 
+read a
+echo "The value of \"a\" is now $a." 
+
+echo 
+
+: '
+output:
+Enter "a" hello
+The value of "a" is now hello.
+
+'
